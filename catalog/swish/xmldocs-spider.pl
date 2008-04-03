defabb7b398dd6a9e0347ce7ccd951b88449bc9e
Jon Jensen <jon@endpoint.com>
2008-04-03 20:44:49 +0000

    Commit stuff people did locally on the new site on elmo.

--- catalog/swish/xmldocs-spider.pl
+++ catalog/swish/xmldocs-spider.pl
@@ -1,10 +1,22 @@
 # spider configuration file for XMLDOCS indexing
 
+sub filter_url {
+	my ($uri) = @_;
+
+	$path = $uri->path;
+	if ($path =~ m%http://(.*?)/+%) {
+		$uri->path($path);
+		warn "Path with double-slashes found: $path\n";
+		return;
+	}
+
+	return 1;
+}
+
 sub filter {
 	my ($uri) = @_;
 
 	if ($uri->path =~ m%/index\.html$%) {
-		warn "Skipping " . $uri->as_string . "\n";
 		return;
 	}
 
@@ -15,6 +27,7 @@ sub filter {
 			{
 			 base_url => 'http://docs.icdevgroup.org/cgi-bin/online/index.html',
 			 email => 'racke@icdevgroup.org',
+			 test_url => \&filter_url,
 			 filter_content => \&filter,
 			}
 			);
