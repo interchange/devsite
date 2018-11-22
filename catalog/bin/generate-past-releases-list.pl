0221822b867b3d6afd00efde2e7967a56b0a3b94
Jon Jensen <jon@endpoint.com>
2018-11-22 01:39:53 +0000

    Move to new domain, HTTPS, UTF-8, single demo

--- catalog/bin/generate-past-releases-list.pl
+++ catalog/bin/generate-past-releases-list.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
-use warnings;
 use strict;
+use warnings;
+use DateTime;
 
-my @files = `find /var/ftp/pub/interchange/ -name "ANNOUNCEMENT-*" | tac`;
+my $base_path = '/home/ftp/public_html';
+my @files = `find $base_path/interchange/ -name "ANNOUNCEMENT-*" | tac`;
 chomp for @files;
 @files = sort { (stat $b)[9] <=> (stat $a)[9] } @files;
 
@@ -12,17 +14,20 @@ my $series;
 for (@files) {
 	my $file = $_;
 	my $mtime = (stat $file)[9];
+	my $timestamp = DateTime->from_epoch(epoch => $mtime)->ymd;
 
-	( my $ftp_ann = $_ ) =~ s#/var/ftp#http://ftp.icdevgroup.org#;
+	( my $ftp_ann = $_ ) =~ s{$base_path}{https://ftp.interchangecommerce.org};
 	( my $ftp_dir = $ftp_ann ) =~ s#ANNOUNC.*##;
 
 	$_ = $file;
 	s/.*ANNOUNCEMENT-(\d+\.\d+.+)\.txt// and $series = $1;
 
-	print
-		'<tr><td>Interchange ' . $series . '</td>'.
-		'<td align="right">' . scalar localtime($mtime) . '</td>' .
-		"<td><a href='$ftp_ann'>Announcement</a></td>" .
-		"<td><a href='$ftp_dir'>Download</a></td></tr>" .
-		"\n";
+	print <<END;
+<tr>
+	<td>Interchange $series</td>
+	<td>$timestamp</td>
+	<td><a href="$ftp_ann">Announcement</a></td>
+	<td><a href="$ftp_dir">Download</a></td>
+</tr>
+END
 }
